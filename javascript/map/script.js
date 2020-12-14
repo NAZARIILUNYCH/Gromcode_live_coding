b7546ace193fa63a57c393fe99efcfdc3454a7e8
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2020-12-14 13:12:02 +0200

    FE-15

--- javascript/map/script.js
+++ javascript/map/script.js
@@ -2,13 +2,12 @@ const mapArrayElements = (arr, callback) => {
   const result = [];
   
    for (let i = 0; i < arr.length; i++) {
-    // callback(arr[i], i, arr);
      result.push(callback(arr[i], i, arr));
    }
   
   return result;
 };
 
-const testCallback = (el, index, arr) => index * 3;
+const testCallback = (el, index, arr) => el * 3;
 
 console.log(mapArrayElements([5, 6, 10, -2, 44, 5], testCallback))
