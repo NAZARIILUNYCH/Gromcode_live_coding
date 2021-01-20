ac36f5d201e4b943314a038428cc0542b4681500
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-01-20 12:45:29 +0200

    f

--- Live_coding/task 1/index.js
+++ Live_coding/task 1/index.js
@@ -1,25 +1,56 @@
-const requestUserData = userId =>
-  new Promise((resolve, reject) => {
-    // console.log('promise is called');
-    // resolve(userId);
-    if (userId === 'broken') {
-      setTimeout(() => {
-        reject(new Error('User not found'));
-      }, 500);
-    } else {
-      setTimeout(() => {
-        resolve({
-          name: 'John',
-          age: 17,
-          userId,
-          email: `${userId}@example.com`,
-        });
-      }, 1000);
-    }
-  });
+/* eslint-disable no-return-assign */
+const battonElem = document.querySelector('.submit-button');
+const errorElem = document.querySelector('.error-text');
+const loginForm = document.querySelector('.login-form');
 
-const res = requestUserData('222');
+const onInputValid = () =>
+  loginForm.reportValidity() ? (battonElem.disabled = false) : (battonElem.disabled = true);
 
-console.log('START');
-console.log(requestUserData(1000));
-console.log('FINISH');
+const submitHandler = event => {
+  event.preventDefault();
+
+  const user = [...new FormData(loginForm)].reduce(
+    (acc, [field, value]) => ({ ...acc, [field]: value }),
+    {},
+  );
+
+  fetch('https://5ff2e7d128c3980017b18ca3.mockapi.io/api/v1/form', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(user),
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.text();
+      }
+
+      errorElem.textContent = 'Failed to create user';
+      throw new Error('Failed to create user');
+    })
+    .then(userData => {
+      loginForm.reset();
+
+      alert(userData);
+    });
+
+  // .then(() => loginForm.reset())
+  // .then(() => getUserForm())
+  // .catch(() => ())
+  // .finally(() => loginForm.addEventListener('input', textError));
+};
+
+const getUserForm = () =>
+  fetch(baseUrl)
+    .then(response => response.json())
+    .then(value => alert(JSON.stringify(value)));
+
+const textError = event => {
+  if (event.type === 'input') {
+    errorElem.textContent = '';
+  }
+};
+
+loginForm.addEventListener('submit', submitHandler);
+loginForm.addEventListener('input', onInputValid);
